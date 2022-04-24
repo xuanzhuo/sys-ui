b75fe75fd4ffa70c242a8d9c9b69ddc32b353cf6
Dhy <763036679@qq.com>
2022-04-24 16:52:45 +0800

    feat: 增加导出

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,7 @@ export { default as SysTabs } from './sys-tabs';
 export { default as SysToolbar } from './sys-toolbar';
 export { default as SysIcon } from './sys-icon';
 export * as SysModal from './sys-modal';
-
+export { default as SysMenu } from './sys-menu';
+export { default as SysInput } from './sys-input';
+export { default as SysSelect } from './sys-select';
+export { default as SysTreeSelect } from './sys-tree-select';
