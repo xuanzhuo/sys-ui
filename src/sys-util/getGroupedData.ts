a015532582bb15fd0c35ab56815d1cc57c15f077
xuanzhuo <631936392@qq.com>
2022-07-28 14:13:18 +0800

    feat: 新增数组数据分组工具函数

---
+++ src/sys-util/getGroupedData.ts
@@ -0,0 +1,53 @@
+/**
+ * 符合表格数据的数据分组
+ * @param dataArray 需要分组的数据
+ * @param config groupedFiled:分组依据的字段;idFiled:rowKey值;groupTitleFormat:分组标题格式
+ * @returns
+ */
+export function getGroupedDataArray(
+    dataArray: Record<string, any>[],
+    {
+        groupedFiled,
+        idFiled,
+        groupTitleFormat,
+    }: {
+        groupedFiled: string;
+        idFiled?: string;
+        groupTitleFormat?: (groupedFiledValue: string, total: number) => string;
+    },
+) {
+    const groupedDataArray: any[] = [];
+    const groupedMap = getGroupedDataMap(dataArray, groupedFiled);
+
+    groupedMap.forEach((value, key) => {
+        const groupedDataArrayItem = {
+            [idFiled || 'id']: `${key}`,
+            [groupedFiled]:
+                (groupTitleFormat && groupTitleFormat(key, value.length)) ||
+                `'${key}(${value.length})'`,
+            children: value,
+        };
+        groupedDataArray.push(groupedDataArrayItem);
+    });
+    return groupedDataArray;
+}
+
+/**
+ * 数据分组
+ * @param dataArray 需要分组的数据
+ * @param groupedFiled 分组依据的字段
+ * @returns 以分组字段为key，对应分组后的数据为value的map
+ */
+export function getGroupedDataMap(dataArray: Record<string, any>[], groupedFiled: string) {
+    const groupedMap = new Map<string, any[]>();
+    for (let i = 0; i < dataArray.length; i++) {
+        const item = dataArray[i];
+        const groupedFiledValue = item[groupedFiled];
+        if (!groupedMap.has(groupedFiledValue)) {
+            groupedMap.set(groupedFiledValue, []);
+        }
+        const groupItems = groupedMap.get(groupedFiledValue) as any[];
+        groupedMap.set(groupedFiledValue, groupItems);
+    }
+    return groupedMap;
+}
--- src/sys-util/index.ts
+++ src/sys-util/index.ts
@@ -1,1 +1,2 @@
 export * from './treeData'
+export * from './getGroupedData'
